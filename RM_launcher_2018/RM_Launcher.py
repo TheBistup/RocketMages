2ec66e07a7dc7674c22bf64b9e09d312bdf79210
OverTimePro <32491132+OverTimePro@users.noreply.github.com>
2018-11-27 20:27:39 +0000

    images and stuff

--- RM_launcher_2018/RM_Launcher.py
+++ RM_launcher_2018/RM_Launcher.py
@@ -10,13 +10,19 @@ clock = pygame.time.Clock()
 
 open = True
 
+#loads background
+background = pygame.image.load('background.jpg')
+def BG():
+    display.blit(background,(0,0))
+title_x = 85
+title_y = 20
 #loading title text image
-title_text = pygame.image.display('title_text.png')
-def RM_t():
-    display.blit(title_text(400,500))
+title_text = pygame.image.load('title_text.png')
+def RM_title():
+    display.blit(title_text,(title_x,title_y))
 
 #game loop
-while open = True:
+while open == True:
     pygame.display.update()
     clock.tick(60)
 #event controller - key press ect...
@@ -25,3 +31,4 @@ while open = True:
             pygame.quit()
             quit()
             open = False
+    RM_title()
