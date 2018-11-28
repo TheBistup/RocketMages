f10dfaf7e147d426a5249e2445e3fea814defdc6
TheBistup <thebistup@null.spigotmc.org>
2018-11-28 19:34:03 +0000

    Net code stuff... again kms

--- game/src/server/server.py
+++ game/src/server/server.py
@@ -12,7 +12,7 @@ class Server():
         with open("server_info.json","r") as file:
             data = json.load(file)
 
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock = socket.socket()
         self.sock.bind((data["ip"], int(data["port"])))
         self.sock.listen(int(data["player_number"]))
         self.clients = []
@@ -25,21 +25,33 @@ class Server():
         self.recieve_init()
 
     def recieve_init(self):
+        print("[!] Get init data from clients")
         self.client_data = []
         for i in range(0, len(self.clients)):
             self.clients[i][0].send(str.encode("+--SEND-INIT--+"))
 
         for i in range(0, len(self.clients)):
             contents = ""
-            data = ""
 
-            while "--+end+--" not in data:
-                data = self.clients[i][0].recv(1024).decode()
-                contents += data
+            contents = self.clients[i][0].recv(1024).decode()
+            self.clients[i][0].send(b'TICK')
+            self.clients[i][0].recv(1024).decode()
 
-            print("[!] Got data from client: ", str(i))
             contents = contents.split("|")
             self.client_data.append([contents[0], contents[1], str(i), 0, 0, "x"])
+
+            print(self.client_data)
+
+        print("[!] Finished initial handshake with clients")
+        while True:
+            self.broadcast_init()
+
+    def broadcast_init(self):
+        for i in range(0, len(self.clients)):
+            data = self.clients[i][0].recv(1024).decode()
+            print("[!] Data from: " + str(self.clients[i][1]) + ": ", data)
+            if data == "--+get-init+--":
+                self.clients[i][0]
 
 
 
