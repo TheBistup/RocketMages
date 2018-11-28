dbef5afdce141d1dd8f16db284d28122e3943a37
OverTimePro <32491132+OverTimePro@users.noreply.github.com>
2018-11-28 20:09:34 +0000

    Launcher UI changes
    UI roughly fits desired design

--- RM_launcher_2018/RM_Launcher.py
+++ RM_launcher_2018/RM_Launcher.py
@@ -23,32 +23,24 @@ green = (0,200,0)
 bright_red = (255,0,0)
 bright_green = (0,255,0)
 
-
-background = pygame.image.load('background.jpg')
-title_text = pygame.image.load('title_text.png')
+image_1 = pygame.image.load('image_1.jpg')
+background = pygame.image.load('UI.png')
+#title_text = pygame.image.load('title_text.png')
 
 #getting texts setup
+smallText = pygame.font.Font("freesansbold.ttf",28)
 font = pygame.font.SysFont(None, 25)
 
-
-
-#loads background
-display.blit(background,(0,0))
-
-
-
 #loading title text image
-title_x = 185
-title_y = 20
+#title_x = 185
+#title_y = 20
 
-def RM_title():
-    display.blit(title_text,(title_x,title_y))
-
+#def RM_title():
+#    display.blit(title_text,(title_x,title_y))
 
 
 #game loop
 while open == True:
-    pygame.display.update()
     display.blit(background,(0,0))
 #event controller - key press ect...
     for event in pygame.event.get():
@@ -56,11 +48,9 @@ while open == True:
             log.log("game closed...")
             pygame.quit()
             quit()
-            open = False
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if 200+150 > mouse_pos[0] > 200 and 450+100 > mouse_pos[1] > 450:
+            if 200+150 > mouse[0] > 200 and 450+100 > mouse[1] > 450:
                 log.log("start button clicked...")
                 os.chdir("../game/src/")
                 os.system("start python main_game.py")
@@ -70,35 +60,37 @@ while open == True:
                 log.log("Game closed by clicking quit...")
                 pygame.quit()
                 quit()
-                open = False
             else:
                 log.log("Mouse button clicked, but nothing happened...[Error 1]")
 
     #get mouse pos and highlights if over button
     mouse = pygame.mouse.get_pos()
-    if 200+150 > mouse[0] > 200 and 450+100 > mouse[1] > 450:
-        pygame.draw.rect(display, bright_green,(200,450,150,100))
-        pygame.draw.rect(display, red,(650,450,150,100))
-    elif 650+150 > mouse[0] > 650 and 450+100 > mouse[1] > 450:
-        pygame.draw.rect(display, bright_red,(650,450,150,100))
-        pygame.draw.rect(display, green,(200,450,150,100))
+    if 690+150 > mouse[0] > 690 and 475+100 > mouse[1] > 475:
+        pygame.draw.rect(display, bright_green,(690,475,150,118))
+        pygame.draw.rect(display, red,(840,475,150,118))
+    elif 840+150 > mouse[0] > 840 and 475+100 > mouse[1] > 475:
+        pygame.draw.rect(display, bright_red,(840,475,150,118))
+        pygame.draw.rect(display, green,(690,475,150,118))
     else:
-        pygame.draw.rect(display, green,(200,450,150,100))
-        pygame.draw.rect(display, red,(650,450,150,100))
+        pygame.draw.rect(display, green,(690,475,150,118))
+        pygame.draw.rect(display, red,(840,475,150,118))
 
-    smallText = pygame.font.Font("freesansbold.ttf",20)
     launch_text = smallText.render("Launch", 1, (255,255,255))
-    display.blit(launch_text,(235, 500))
+    display.blit(launch_text,(690+(150/7), 525))
+
+    display.blit(image_1,(1,1))
 
     exit_text = smallText.render("Quit",1,(255,255,255))
-    display.blit(exit_text,(700, 500))
+    display.blit(exit_text,(840+(150/5), 525))
+
+
 
     fps = font.render(str(int(clock.get_fps())), True, pygame.Color('white'))
     display.blit(fps, (10, 10))
 
-    clock.tick(120)
+    clock.tick(144)
 
-    RM_title()
+    #RM_title()
 
 
     pygame.display.update()
